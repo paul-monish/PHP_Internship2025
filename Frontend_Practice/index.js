b031ffd186c8ece2a2c80bd34f8b7d0e7d4911c4
paul-monish <monish.paul2000@gmail.com>
2025-07-21 19:31:15 +0530

    working on frontend js

---
+++ Frontend_Practice/index.js
@@ -0,0 +1,77 @@
+// console.log("Hello World");
+// var a=5;
+// function test(){
+//     var a=6;
+//     a=a+2;
+
+
+
+// console.log(a);
+// }
+
+// test();
+
+
+// console.log(a);
+// let a=5;
+
+// let a=5;
+// function test(){
+//     let a=5;
+//     a=a+5;
+// }
+
+// test();
+// console.log(a);
+
+
+// let a=5;
+// let b='5';
+
+// console.log(a===b);
+
+// String
+// Number
+// BigInt
+// Boolean
+// undefined
+// null
+// Symbol
+// Object
+
+// let a=undefined;
+// console.log(typeof a);
+
+const arr = [4, 5, 6, 7, 8];
+// console.log(a[3]);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+
+// }
+// for(let a of arr ){
+//     console.log(a); 
+// }
+
+// for(let a in arr){
+//     console.log(arr[a]);
+// }
+// 0=>1,1=>2
+// const person={fname:"Jhone",lane:"Doe",age:"25"};
+// for(let x in person){
+//     console.log(person[x]);
+// }
+
+// function print(a) {
+//     console.log(a);
+// }
+
+// const print = function (a) {
+// console.log(a);
+// }
+
+// const print=(a)=>{
+//     console.log(a);
+// }
+
+arr.forEach((a) =>console.log(a));
